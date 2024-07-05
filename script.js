b13feceec15e81bbcd4409f1dcbaed260c610763
Shiva510pandey <shivanshpandey747@gmail.com>
2024-07-05 13:01:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-}
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
 
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Form submitted successfully!');
-});
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
